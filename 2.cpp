2b0d20ec3df41a178ae55a5d03bb05bb7d3a47e8
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2022-05-02 22:42:05 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -57,3 +57,61 @@ output:-
   **
  ***
 ****
+
+HALF PYRAMID USING NUMBERS
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+    return 0;
+}
+
+output:-
+5
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+
+FLOYD'S TRIANGLE:-
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+int count=0;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+       cout<<count<<" ";
+       count++;
+    }
+    cout<<endl;
+}
+    return 0;
+}
+
+output:-
+5
+0 
+1 2 
+3 4 5 
+6 7 8 9 
+10 11 12 13 14 
